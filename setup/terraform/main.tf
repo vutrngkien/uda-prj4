01e644d2d4d25ffbdf6728acb33948fc76c9d8c8
vutrngkien <vutrungkien2k@gmail.com>
2024-10-28 10:20:42 +0700

    update provider

--- setup/terraform/main.tf
+++ setup/terraform/main.tf
@@ -328,3 +328,10 @@ data "aws_iam_policy_document" "github_policy" {
     resources = ["*"]
   }
 }
+
+provider "kubernetes" {	
+  load_config_file       = "false"
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  token                  = data.aws_eks_cluster_auth.cluster.token
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+}
